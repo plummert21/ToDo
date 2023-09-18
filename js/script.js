242f6cf7325ba073df2949cf52601c577e7d7839
Ваше Имя <you@example.com>
2023-09-18 17:19:01 +0500

    add localstorage

--- js/script.js
+++ js/script.js
@@ -3,26 +3,41 @@ const inputBtn = document.getElementById('inputBtn');
 const todoField = document.getElementById('field');
 
 const TaskList = [
-    {
-        text: 'HTML',
-        isDone: true
-    },
-    {
-        text: 'CSS',
-        isDone: false
-    },
-    {
-        text: 'JS',
-        isDone: false
-    }
+    // {
+    //     text: 'HTML',
+    //     isDone: true
+    // },
+    // {
+    //     text: 'CSS',
+    //     isDone: false
+    // },
+    // {
+    //     text: 'JS',
+    //     isDone: false
+    // }
 ]
+
+// сохранение в Storage
+function addLocalStorage() {
+    localStorage.setItem("ToDoList", JSON.stringify(TaskList));
+}
+
+// инициализация TaskList (значения из Storage при первом открытии страницы)
+function initTaskList() {
+    if (JSON.parse(localStorage.getItem("ToDoList"))) {
+        JSON.parse(localStorage.getItem("ToDoList")).forEach(function (item) {
+            TaskList.push(item);
+        })
+    }
+}
 
 function addItemInTaskList() {
     const item = {
         text: inputText.value,
         isDone: false
     }
-    TaskList.push(item[0]);
+    TaskList.push(item);
+    addJSON();
 }
 
 function render() {
@@ -32,23 +47,23 @@ function render() {
     })
 }
 
-function generateFieldTask(obj, index) {
+function generateFieldTask(item, index) {
     // создание 1 элемента ToDo
     const elFieldTask = document.createElement('div');
     elFieldTask.classList.add('todo-field__item');
-    if (obj.isDone) {
+    if (item.isDone) {
         elFieldTask.classList.add('todo-field__item-done')
     }
     // создание checkbox'а
     const taskCheckbox = document.createElement('input');
     taskCheckbox.type = "checkbox";
     taskCheckbox.classList.add('todo-field__item-input');
-    taskCheckbox.checked = obj.isDone;;
+    taskCheckbox.checked = item.isDone;;
     taskCheckbox.setAttribute('data-number', index);
     // создание текстового поля
     const taskText = document.createElement('p');
     taskText.classList.add('todo-field__item-text');
-    taskText.innerText = obj.text;
+    taskText.innerText = item.text;
     taskText.setAttribute('data-number', index);
     // создание кнопки для удаления элемента Todo
     const taskImg = document.createElement('img');
@@ -66,15 +81,18 @@ function generateFieldTask(obj, index) {
     //// выполнение записи (нажатие checkbox)
     taskCheckbox.addEventListener(('change'), () => {
         TaskList[taskCheckbox.dataset.number].isDone = taskCheckbox.checked;
+        addLocalStorage();
         render();
     })
     //// удаление записи (нажатие img trash)
     taskImg.addEventListener(('click'), () => {
         TaskList.splice(taskImg.dataset.number, 1);
+        addLocalStorage();
         render();
     })
 }
 
+// добавление таска (нажатие кнопки Добавить)
 inputBtn.addEventListener('click', () => {
     if (inputText.value.trim()) {
         addItemInTaskList();
@@ -83,4 +101,5 @@ inputBtn.addEventListener('click', () => {
     render();
 })
 
+initTaskList();
 render();
