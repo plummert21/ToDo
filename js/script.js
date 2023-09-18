717eee347a98383dd54e1cfd2da16b6e92574503
Ваше Имя <you@example.com>
2023-09-18 09:34:16 +0500

    smch

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const inputText = document.getElementById('inputText')
-const inputBtn = document.getElementById('inputBtn')
-const todoField = document.getElementById('field')
+const inputText = document.getElementById('inputText');
+const inputBtn = document.getElementById('inputBtn');
+const todoField = document.getElementById('field');
 
 const TaskList = [
     {
@@ -24,83 +24,79 @@ function addItemInTaskList() {
             isDone: false
         }
     ]
-    TaskList.push(item[0])
+    TaskList.push(item[0]);
 }
 
 function render() {
-    todoField.innerHTML = ''
-    TaskList.forEach(function (item) {
-        generateFieldTask(item)
+    todoField.innerHTML = '';
+    TaskList.forEach(function (item, index) {
+        generateFieldTask(item, index);
     })
 }
 
-function generateFieldTask(obj) {
-    const elFieldTask = document.createElement('div')
-    elFieldTask.classList.add('todo-field__item')
+function generateFieldTask(obj, index) {
+    // создание 1 элемента ToDo
+    const elFieldTask = document.createElement('div');
+    elFieldTask.classList.add('todo-field__item');
     if (obj.isDone) {
         elFieldTask.classList.add('todo-field__item-done')
     }
-    todoField.appendChild(elFieldTask)
-    const taskCheckbox = document.createElement('input')
-    taskCheckbox.type = "checkbox"
-    taskCheckbox.classList.add('todo-field__item-input')
-    if (obj.isDone) {
-        taskCheckbox.checked = true
-    }
-    elFieldTask.appendChild(taskCheckbox)
-    const taskText = document.createElement('p')
-    taskText.classList.add('todo-field__item-text')
-    taskText.innerText = obj.text
-    elFieldTask.appendChild(taskText)
-    const taskImg = document.createElement('img')
-    taskImg.classList.add('todo-field__item-trash')
-    taskImg.src = "/img/trash.svg"
-    taskImg.alt = "Удалить"
-    elFieldTask.appendChild(taskImg)
-
+    // создание checkbox'а
+    const taskCheckbox = document.createElement('input');
+    taskCheckbox.type = "checkbox";
+    taskCheckbox.classList.add('todo-field__item-input');
+    taskCheckbox.checked = obj.isDone;;
+    taskCheckbox.setAttribute('data-number', index);
+    // checkbox.addEventListener('change', () => {
+    //     //     const isChecked = checkbox.checked;
+    //     //     if (isChecked) {
+    //     //         elFieldTask.classList.add('todo-field__item-done');
+    //     //         TaskList[index].isDone = true
+    //     //     } else {
+    //     //         elFieldTask.classList.remove('todo-field__item-done');
+    //     //         TaskList[index].isDone = false
+    //     //     }
+    // })
+    // создание текстового поля
+    const taskText = document.createElement('p');
+    taskText.classList.add('todo-field__item-text');
+    taskText.innerText = obj.text;
+    // создание кнопки для удаления элемента Todo
+    const taskImg = document.createElement('img');
+    taskImg.classList.add('todo-field__item-trash');
+    taskImg.src = "/img/trash.svg";
+    taskImg.alt = "Удалить";
+    // Добавление родителя для 3 элементов блока
+    elFieldTask.appendChild(taskCheckbox);
+    elFieldTask.appendChild(taskText);
+    elFieldTask.appendChild(taskImg);
+    // Добавление родителя для блока item (отображение на странице)
+    todoField.appendChild(elFieldTask);
 }
 
 inputBtn.addEventListener('click', () => {
     if (inputText.value.trim()) {
-        addItemInTaskList()
-        inputText.value = ''
+        addItemInTaskList();
+        inputText.value = '';
     }
-    render()
+    render();
 })
 
-document.addEventListener('DOMContentLoaded', function () {
-    const taskCheckboxes = document.querySelectorAll('.todo-field .todo-field__item .todo-field__item-input');
-    taskCheckboxes.forEach((checkbox, index) => {
-        checkbox.addEventListener('change', () => {
-            const isChecked = event.target.checked;
-            const taskItems = document.querySelectorAll('.todo-field .todo-field__item');
-            if (isChecked) {
-                taskItems[index].classList.add('todo-field__item-done');
-                TaskList[index].isDone = true
-            } else {
-                taskItems[index].classList.remove('todo-field__item-done');
-                TaskList[index].isDone = false
-            }
-        })
-    })
-})
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const checkboxes = document.querySelectorAll('.task-list-container .task-checkbox'); // Обновляем селектор
-
-//     checkboxes.forEach((checkbox, index) => {
-//         checkbox.addEventListener('change', (event) => {
+// document.addEventListener('DOMContentLoaded', function () {
+//     const taskCheckboxes = document.querySelectorAll('.todo-field .todo-field__item .todo-field__item-input');
+//     taskCheckboxes.forEach((checkbox, index) => {
+//         checkbox.addEventListener('change', () => {
 //             const isChecked = event.target.checked;
-//             const labels = document.querySelectorAll('.task-list-container .task-label'); // Обновляем селектор
+//             const taskItems = document.querySelectorAll('.todo-field .todo-field__item');
 //             if (isChecked) {
-//                 labels[index].classList.add('task-label-done');
+//                 taskItems[index].classList.add('todo-field__item-done');
+//                 TaskList[index].isDone = true
 //             } else {
-//                 labels[index].classList.remove('task-label-done');
+//                 taskItems[index].classList.remove('todo-field__item-done');
+//                 TaskList[index].isDone = false
 //             }
-//         });
-//     });
-// });
+//         })
+//     })
+// })
 
-// const taskTrashes = document.querySelectorAll('.todo-field__item .todo-field__item-trash');
-
-render()
+render();
